f54d2cb7bec4e436e7775f5252b5908c446c9484
Fancy <linuxfj@gmail.com>
2015-10-13 21:03:53 +0800

    1

--- main.go
+++ main.go
@@ -1,116 +1,118 @@
 package main
+
 import (
-    "fmt"
-    "html/template"
-    "net/http"
-    "log"
-    "os"
-    "io"
-    "io/ioutil"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 const (
-    UPLOAD_DIR = "./upload"
+	UPLOAD_DIR = "./upload"
 )
 
-func sayHelloName( w http.ResponseWriter, r *http.Request ) {
-    r.ParseForm()
-    //fmt.Fprintf( w, "快来看我的网页 = =0" )
-    t, _ := template.ParseFiles( "main_page.html" )
-    t.Execute( w, nil )
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	//fmt.Fprintf( w, "快来看我的网页 = =0" )
+	t, _ := template.ParseFiles("main_page.html")
+	t.Execute(w, nil)
 }
 
-func login( w http.ResponseWriter, r *http.Request ) {
-    fmt.Println( "method:", r.Method )
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles( "login.html" )
-        t.Execute( w, nil )
-    } else {
-        r.ParseForm()
-        fmt.Println( "username", r.Form["username"] )
-        fmt.Println( "password", r.Form["password"] )
-    }
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username", r.Form["username"])
+		fmt.Println("password", r.Form["password"])
+	}
 }
 
-func upload( w http.ResponseWriter, r *http.Request ) {
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles( "upload.html" )
-        t.Execute( w, nil )
-    }
-    if r.Method == "POST" {
-        f, h, err := r.FormFile( "image" )
-        if err != nil {
-            http.Error( w, err.Error(),
-            http.StatusInternalServerError )
-            return
-        }
-        filename := h.Filename
-        defer f.Close()
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, nil)
+	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
 
-        t, err := os.Create( UPLOAD_DIR + "/" + filename )
-        if err != nil {
-            http.Error( w, err.Error(),
-            http.StatusInternalServerError )
-            return
-        }
-        defer t.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
 
-        if _, err := io.Copy( t, f ); err != nil {
-            http.Error( w, err.Error(),
-            http.StatusInternalServerError )
-            return
-        }
-        http.Redirect( w, r, "/view?id="+filename,
-        http.StatusFound )
-    }
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(),
+				http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id="+filename,
+			http.StatusFound)
+	}
 }
 
-func view( w http.ResponseWriter, r *http.Request ) {
-    imageId := r.FormValue( "id" )
-    imagePath := UPLOAD_DIR + "/" + imageId
-    if exists := isexist( imagePath ); !exists {
-        http.NotFound( w, r )
-    }
-    w.Header().Set( "Content-Type", "image")
-    http.ServeFile( w, r, imagePath )
+func view(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isexist(imagePath); !exists {
+		http.NotFound(w, r)
+	}
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
 }
 
-func isexist( path string ) bool {
-    _, err := os.Stat( path )
-    if err == nil {
-        return true
-    }
-    return os.IsExist( err )
+func isexist(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
 }
 
-func list( w http.ResponseWriter, r *http.Request ) {
-    fileInfoArr, err := ioutil.ReadDir( "./upload" )
-    if err != nil {
-        http.Error( w, err.Error(),
-        http.StatusInternalServerError )
-        return
-    }
-    locals := make( map[string]interface{} )
-    images := []string{}
-    for _, fileinfo := range fileInfoArr {
-        images = append( images, fileInfo.Name() )
-    }
-    locals["image"] = images
-    t, err := template.ParseFiles( "list.html" )
-    if err != nil {
-        http.Error( w, err.Error(), http.StatusInternalServerError )
-    }
-    t.Excute( w, locals )
+func list(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir("./upload")
+	if err != nil {
+		http.Error(w, err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	locals := make(map[string]interface{})
+	images := []string{}
+	for _, fileinfo := range fileInfoArr {
+		images = append(images, fileInfo.Name())
+	}
+	locals["image"] = images
+	t, err := template.ParseFiles("list.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	t.Excute(w, locals)
 }
 
 func main() {
-    http.HandleFunc( "/", sayHelloName )
-    http.HandleFunc( "/login", login )
-    http.HandleFunc( "/upload", upload )
-    http.HandleFunc( "/view", view )
-    http.HandleFunc( "/list", view )
-    err := http.ListenAndServe( ":80", nil )
-    if err != nil {
-        log.Fatal( "ListenAndServe:", err )
-    }
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/view", view)
+	http.HandleFunc("/list", view)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+		log.Debug("fancy")
+	}
 }
